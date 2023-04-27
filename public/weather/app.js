6dabff46e83db9113cbfb9f6e3e1a6ef73a5e90d
areej206 <99836322+areej206@users.noreply.github.com>
2023-04-27 23:24:24 +0100

    FOR MADIHAH

--- public/weather/app.js
+++ public/weather/app.js
@@ -30,10 +30,9 @@ if('geolocation' in navigator){
 function setPosition(position){
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-
+    
     getWeather(latitude, longitude);
 }
-
 
 // SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
 function showError(error){
