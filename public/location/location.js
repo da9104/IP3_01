94444884932d0612a7d1899556014a90caf49265
IonaHiggins <ihiggi200@caledonian.ac.uk>
2023-04-28 16:39:51 +0100

    Deleted extra files, fixed script issues

--- public/location/location.js
+++ public/location/location.js
@@ -1,11 +1,27 @@
+console.log("location");
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError);
   }else{
     notificationElement.style.display = "block";
     notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+}
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
   }
-  
+}  
   // SET USER'S POSITION
   function setPosition(position){
     let latitude = position.coords.latitude;
