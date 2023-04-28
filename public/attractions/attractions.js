210cd291b507d5441a8d66457c60e475494d6527
IonaHiggins <ihiggi200@caledonian.ac.uk>
2023-04-28 05:30:31 +0100

    Added stuff

--- public/attractions/attractions.js
+++ public/attractions/attractions.js
@@ -1,3 +1,5 @@
+console.log("test 1");
+console.log("test x");
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
   navigator.geolocation.getCurrentPosition(setPosition, showError);
@@ -7,6 +9,7 @@ if('geolocation' in navigator){
 }
 
 // SET USER'S POSITION
+console.log("test 2");
 function setPosition(position){
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
@@ -17,6 +20,7 @@ function setPosition(position){
 }
 
 function getAttractions(latitude,longitude){
+  console.log("test 3");
 
   const apiKey = `5ae2e3f221c38a28845f05b6e0fd8459aa247818bb4d27e011639812`
 
@@ -26,12 +30,30 @@ function getAttractions(latitude,longitude){
      let attraction3 =`${data[2].name}`;
      let attraction4 = `${data[3].name}`;
      let attraction5 = `${data[4].name}`;
-      //` Distance: `+`${data[i].dist}`+
- // ` Rating: `+`${data[i].rate}`
 
-      console.log(attraction1,attraction2,attraction3,attraction4,attraction5);
+     let attraction6 =`Name: `+ ` `+ `${data[0].name}`+ ` `+
+      ` Distance: `+ ` `+`${data[0].dist}`+ ` `+
+      ` Rating: `+` `+`${data[0].rate}` +`.`;
 
-      displayAttractions(attraction1,attraction2,attraction3,attraction4,attraction5);
+      let attraction7 =`Name: `+ ` `+ `${data[1].name}`+ ` `+
+      ` Distance: `+ ` `+`${data[1].dist}`+ ` `+
+      ` Rating: `+` `+`${data[1].rate}` +`.`;
+
+      let attraction8 =`Name: `+ ` `+ `${data[2].name}`+ ` `+
+      ` Distance: `+ ` `+`${data[2].dist}`+ ` `+
+      ` Rating: `+` `+`${data[2].rate}` +`.`;
+
+      let attraction9 =`Name: `+ ` `+ `${data[3].name}`+ ` `+
+      ` Distance: `+ ` `+`${data[3].dist}`+ ` `+
+      ` Rating: `+` `+`${data[3].rate}` +`.`;
+      
+      let attraction10 =`Name: `+ ` `+ `${data[4].name}`+ ` `+
+      ` Distance: `+ ` `+`${data[4].dist}`+ ` `+
+      ` Rating: `+` `+`${data[4].rate}` +`.`;
+
+      console.log(attraction1,attraction2,attraction3,attraction4,attraction5,attraction6,attraction7,attraction8,attraction9,attraction10);
+
+      displayAttractions(attraction1,attraction2,attraction3,attraction4,attraction5,attraction6,attraction7,attraction8,attraction9,attraction10);
 })
 
 }
@@ -44,10 +66,23 @@ function displayAttractions(attraction1,attraction2,attraction3,attraction4,attr
   const att4Element = document.querySelector(`.attraction4`);
   const att5Element = document.querySelector(`.attraction5`);
 
+  const att6Element = document.querySelector(`.attraction6`);
+  const att7Element = document.querySelector(`.attraction7`);
+  const att8Element = document.querySelector(`.attraction8`);
+  const att9Element = document.querySelector(`.attraction9`);
+  const att10Element = document.querySelector(`.attraction10`);
+
+
   att1Element.innerHTML = `${attraction1}`;
   att2Element.innerHTML = `${attraction2}`;
   att3Element.innerHTML = `${attraction3}`;
   att4Element.innerHTML = `${attraction4}`;
   att5Element.innerHTML = `${attraction5}`;
-  
+
+  att6Element.innerHTML = `${attraction6}`;
+  att7Element.innerHTML = `${attraction7}`;
+  att8Element.innerHTML = `${attraction8}`;
+  att9Element.innerHTML = `${attraction9}`;
+  att10Element.innerHTML = `${attraction10}`;
+  console.log("test 4");
 }
