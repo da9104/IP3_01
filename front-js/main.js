8ced1a27aa9b676d5d01050ec74aa8f02c90f941
madihah99 <madihah.ahmad@icloud.com>
2023-04-28 06:53:28 +0100

    menu bar

--- front-js/main.js
+++ front-js/main.js
@@ -1,16 +1,47 @@
 import Search from "./modules/search";
-import Chat from "./modules/chat"
-import RegistrationForm from "./modules/registrationForm"
-
+import Chat from "./modules/chat";
+import RegistrationForm from "./modules/registrationForm";
 
 if (document.querySelector("#registration-form")) {
-    new RegistrationForm()
+  new RegistrationForm();
 }
 
 if (document.querySelector(".header-search-icon")) {
-    new Search()
+  new Search();
 }
 
 if (document.querySelector("#chat-wrapper")) {
-    new Chat()
+  new Chat();
 }
+
+/**
+ * This code is for the nav link menu which will show on screen with width 800px or less
+ */
+//calls to change menu icon from burger to X when clicked on
+function menuChange(x) {
+  x.classList.toggle("change");
+}
+
+//code for the mobile/smaller screen nav links in menu
+const menuSlide = () => {
+  const menu = document.querySelector(".menu-icon");
+  const nav = document.querySelector(".nav_links ");
+  const navLinks = document.querySelectorAll(".nav_links li");
+
+  //toogles the menu
+  menu.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    //animates the links to appear on menu
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+      console.log(index);
+    });
+  });
+};
+
+//calls the menuSlide() function
+menuSlide();
